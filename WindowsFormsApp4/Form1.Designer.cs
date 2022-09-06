dda48d88877e32d5e1e15b961059acdca4a09cfb
«Evgenia» <ekadolo@mail.ru>
2022-09-06 21:19:36 +0500

    MathFunctions

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(464, 351);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Построить график";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(168, 340);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(168, 365);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(168, 417);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "y = x * a + b",
+            "y = a * sin(x * b) + c",
+            "y = a * cos(x * b + c)",
+            "y = a * tg(x * b) + c",
+            "y = a * ctg(x * b + c)",
+            "y = b * x^a",
+            "y = a^(x + b)",
+            "y = ((sin(x) + a * x)/((x – (x)^1/2) + 1))^b/x"});
+            this.comboBox1.Location = new System.Drawing.Point(168, 313);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(182, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(149, 341);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(41, 314);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Выберите функцию";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(149, 366);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "b";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(149, 418);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "x";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(149, 392);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "c";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(168, 391);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedItem = "y = x * a + b";
+        }
+
+        Chart myChart = new Chart();
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a;
+                double b;
+                double c;
+
+                myChart.ChartAreas.Clear();
+                myChart.Series.Clear();
+
+                myChart.Parent = this;
+                myChart.Dock = DockStyle.Top;
+                myChart.ChartAreas.Add(new ChartArea("Math functions"));
+                Series mySeriesOfPoint = new Series();
+                mySeriesOfPoint.Color = Color.Red;
+                mySeriesOfPoint.ChartType = SeriesChartType.Line;
+                mySeriesOfPoint.ChartArea = "Math functions";
+
+                if (textBox3.Text != "")
+                {
+                    double x = double.Parse(textBox3.Text);
+                    mySeriesOfPoint.ChartType = SeriesChartType.Point;
+                    switch (comboBox1.SelectedIndex)
+                    {
+                        case 0:
+                            a = double.Parse(textBox1.Text);
+                            b = double.Parse(textBox2.Text);
+                            mySeriesOfPoint.Points.AddXY(x, x * a + b);
+                            break;
+                        case 1:
+                            a = double.Parse(textBox1.Text);
+                            b = double.Parse(textBox2.Text);
+                            c = double.Parse(textBox4.Text);
+                            mySeriesOfPoint.Points.AddXY(x, a * Math.Sin(x * b) + c);
+                            break;
+                        case 2:
+                            a = double.Parse(textBox1.Text);
+                            b = double.Parse(textBox2.Text);
+                            c = double.Parse(textBox4.Text);
+                            mySeriesOfPoint.Points.AddXY(x, a * Math.Cos(x * b + c));
+                            break;
+                        case 3:
+                            a = double.Parse(textBox1.Text);
+                            b = double.Parse(textBox2.Text);
+                            c = double.Parse(textBox4.Text);
+                            mySeriesOfPoint.Points.AddXY(x, a * Math.Tan(x * b) + c);
+                            break;
+                        case 4:
+                            a = double.Parse(textBox1.Text);
+                            b = double.Parse(textBox2.Text);
+                            c = double.Parse(textBox4.Text);
+                            mySeriesOfPoint.Points.AddXY(x, a * 1 / Math.Tan(x * b + c));
+                            break;
+                        case 5:
+                            a = double.Parse(textBox1.Text);
+                            b = double.Parse(textBox2.Text);
+                            mySeriesOfPoint.Points.AddXY(x, b * Math.Pow(x, a));
+                            break;
+                        case 6:
+                            a = double.Parse(textBox1.Text);
+                            b = double.Parse(textBox2.Text);
+                            mySeriesOfPoint.Points.AddXY(x, Math.Pow(a, x + b));
+                            break;
+                        case 7:
+                            a = double.Parse(textBox1.Text);
+                            b = double.Parse(textBox2.Text);
+                            if ((a < 7 && a > 0 && b == 1) || (b < 3 && b > -1 && a == 1))
+                                mySeriesOfPoint.Points.AddXY(x, Math.Pow((Math.Sin(x) + a * x) / (x - Math.Pow(x, 1 / 2) + 1), b / x));
+                            break;
+                        default:
+                            break;
+                    }
+                    myChart.Series.Add(mySeriesOfPoint);
+                }
+                else
+                {
+                    for (double x = -Math.PI; x <= Math.PI; x += Math.PI / 100.0)
+                    {
+                        switch (comboBox1.SelectedIndex)
+                        {
+                            case 0:
+                                a = double.Parse(textBox1.Text);
+                                b = double.Parse(textBox2.Text);
+                                mySeriesOfPoint.Points.AddXY(x, x * a + b);
+                                break;
+                            case 1:
+                                a = double.Parse(textBox1.Text);
+                                b = double.Parse(textBox2.Text);
+                                c = double.Parse(textBox4.Text);
+                                mySeriesOfPoint.Points.AddXY(x, a * Math.Sin(x * b) + c);
+                                break;
+                            case 2:
+                                a = double.Parse(textBox1.Text);
+                                b = double.Parse(textBox2.Text);
+                                c = double.Parse(textBox4.Text);
+                                mySeriesOfPoint.Points.AddXY(x, a * Math.Cos(x * b + c));
+                                break;
+                            case 5:
+                                a = double.Parse(textBox1.Text);
+                                b = double.Parse(textBox2.Text);
+                                mySeriesOfPoint.Points.AddXY(x, b * Math.Pow(x, a));
+                                break;
+                            case 6:
+                                a = double.Parse(textBox1.Text);
+                                b = double.Parse(textBox2.Text);
+                                mySeriesOfPoint.Points.AddXY(x, Math.Pow(a, x + b));
+                                break;
+                            case 7:
+                                a = double.Parse(textBox1.Text);
+                                b = double.Parse(textBox2.Text);
+                                if((a < 7 && a > 0 && b == 1) || (b < 3 && b > -1 && a == 1))
+                                    mySeriesOfPoint.Points.AddXY(x, Math.Pow((Math.Sin(x) + a * x) / (x - Math.Pow(x, 1 / 2) + 1), b / x));
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    myChart.Series.Add(mySeriesOfPoint);
+                    if(comboBox1.SelectedIndex == 3) funcTan();
+                    if(comboBox1.SelectedIndex == 4) funcCtg();
+                }
+            }
+            catch
+            {}
+        }
+
+        private void funcTan()
+        {
+            var a = double.Parse(textBox1.Text);
+            var b = double.Parse(textBox2.Text);
+            var c = double.Parse(textBox4.Text);
+
+            List<Series> series = new List<Series>(4);
+
+            for (int i = 0; i < series.Capacity; i++)
+            {
+                series.Add(new Series());
+                series[i].Color = Color.Red;
+                series[i].ChartType = SeriesChartType.Line;
+                series[i].ChartArea = "Math functions";
+            }
+
+            if(b < 0)
+            {
+                for (double x = Math.PI / 2.1 / b; x <= -Math.PI / 2.08 / b; x += Math.PI / 10000.0)
+                {
+                    series[0].Points.AddXY(x, a * Math.Tan(x * b) + c);
+                }
+                for (double y = 3 * Math.PI / 2.035 / b; y <= Math.PI / 1.93 / b; y += Math.PI / 10000.0)
+                {
+                    series[1].Points.AddXY(y, a * Math.Tan(y * b) + c);
+                }
+                for (double y = 5 * Math.PI / 2.022 / b; y <= 3 * Math.PI / 1.975 / b; y += Math.PI / 10000.0)
+                {
+                    series[2].Points.AddXY(y, a * Math.Tan(y * b) + c);
+                }
+                for (double y = 7 * Math.PI / 2.1 / b; y <= 5 * Math.PI / 1.985 / b; y += Math.PI / 10000.0)
+                {
+                    series[3].Points.AddXY(y, a * Math.Tan(y * b) + c);
+                }
+            }
+            else if(b > 0)
+            {
+                for (double x = -Math.PI / 2.08 / b; x <= Math.PI / 2.1 / b; x += Math.PI / 10000.0)
+                {
+                    series[0].Points.AddXY(x, a * Math.Tan(x * b) + c);
+                }
+                for (double y = Math.PI / 1.93 / b; y <= 3 * Math.PI / 2.035 / b; y += Math.PI / 10000.0)
+                {
+                    series[1].Points.AddXY(y, a * Math.Tan(y * b) + c);
+                }
+                for (double y = 3 * Math.PI / 1.975 / b; y <= 5 * Math.PI / 2.022 / b; y += Math.PI / 10000.0)
+                {
+                    series[2].Points.AddXY(y, a * Math.Tan(y * b) + c);
+                }
+                for (double y = 5 * Math.PI / 1.985 / b; y <= 7 * Math.PI / 2.1 / b; y += Math.PI / 10000.0)
+                {
+                    series[3].Points.AddXY(y, a * Math.Tan(y * b) + c);
+                }
+            }
+            else
+            {
+                for (double x = -Math.PI / 2.08; x <= Math.PI / 2.1; x += Math.PI / 10000.0)
+                {
+                    series[0].Points.AddXY(x, a * Math.Tan(x * b) + c);
+                }
+            }
+            myChart.Series.Add(series[0]);
+            myChart.Series.Add(series[1]);
+            myChart.Series.Add(series[2]);
+            myChart.Series.Add(series[3]);
+        }
+
+        private void funcCtg()
+        {
+            var a = double.Parse(textBox1.Text);
+            var b = double.Parse(textBox2.Text);
+            var c = double.Parse(textBox4.Text);
+
+            List<Series> series = new List<Series>(4);
+
+            for (int i = 0; i < series.Capacity; i++)
+            {
+                series.Add(new Series());
+                series[i].Color = Color.Red;
+                series[i].ChartType = SeriesChartType.Line;
+                series[i].ChartArea = "Math functions";
+            }
+
+            if (b > 0)
+            {
+                for (double x = (-0.99 * Math.PI - c) / b; x <= (-0.01 - c) / b; x += Math.PI / 10000.0)
+                {
+                    series[0].Points.AddXY(x, a * 1 / Math.Tan(x * b + c));
+                }
+                for (double x = (0.01 - c) / b; x <= (Math.PI - 0.01 - c) / b; x += Math.PI / 10000.0)
+                {
+                    series[1].Points.AddXY(x, a * 1 / Math.Tan(x * b + c));
+                }
+                for (double x = (3.151 - c) / b; x <= (6.273 - c) / b; x += Math.PI / 10000.0)
+                {
+                    series[2].Points.AddXY(x, a * 1 / Math.Tan(x * b + c));
+                }
+                for (double x = (6.292 - c) / b; x <= (9.415 - c) / b; x += Math.PI / 10000.0)
+                {
+                    series[3].Points.AddXY(x, a * 1 / Math.Tan(x * b + c));
+                }
+            }
+            else if (b < 0)
+            {
+                for (double x = (-0.01 - c) / b; x <= (-0.99 * Math.PI - c) / b; x += Math.PI / 10000.0)
+                {
+                    series[0].Points.AddXY(x, a * 1 / Math.Tan(x * b + c));
+                }
+                for (double x = (Math.PI - 0.01 - c) / b; x <= (0.01 - c) / b; x += Math.PI / 10000.0)
+                {
+                    series[1].Points.AddXY(x, a * 1 / Math.Tan(x * b + c));
+                }
+                for (double x = (6.273 - c) / b; x <= (3.151 - c) / b; x += Math.PI / 10000.0)
+                {
+                    series[2].Points.AddXY(x, a * 1 / Math.Tan(x * b + c));
+                }
+                for (double x = (9.415 - c) / b; x <= (6.292 - c) / b; x += Math.PI / 10000.0)
+                {
+                    series[3].Points.AddXY(x, a * 1 / Math.Tan(x * b + c));
+                }
+            }
+            else
+            {
+                for (double x = -0.99 * Math.PI - c; x <= -0.01 - c; x += Math.PI / 10000.0)
+                {
+                    series[0].Points.AddXY(x, a * 1 / Math.Tan(x * b + c));
+                }
+            }
+            
+            myChart.Series.Add(series[0]);
+            myChart.Series.Add(series[1]);
+            myChart.Series.Add(series[2]);
+            myChart.Series.Add(series[3]);
+        }
+    }
+}
